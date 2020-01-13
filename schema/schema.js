0a3d59b4678f5c7d51b5faa6c18b50c7c1502cff
shmohamud <shmohamud@email.wm.edu>
2020-01-12 21:47:10 -0500

    new queries, refactor

--- schema/schema.js
+++ schema/schema.js
@@ -2,8 +2,6 @@ const graphql = require("graphql");
 const fetch = require("node-fetch");
 const { JsonRpc } = require("eosjs");
 
-//access "full" eos block-level data
-
 const rpc = new JsonRpc("http://api.eosnewyork.io", { fetch });
 const {
   GraphQLObjectType,
@@ -13,15 +11,12 @@ const {
   GraphQLList
 } = graphql;
 
-//----------------------------------------------NON-ROOT-TYPES---------------------------------------------------//
-
 const ChainType = new GraphQLObjectType({
   name: "Chain",
   fields: () => ({
     server_version: { type: GraphQLString },
     server_version_string: { type: GraphQLString },
     chain_id: { type: GraphQLString },
-    head_block_id: { type: GraphQLInt },
     last_irreversible_block_id: { type: GraphQLString },
     last_irreversible_block_num: { type: GraphQLInt },
     head_block_id: { type: GraphQLString },
@@ -36,7 +31,6 @@ const ChainType = new GraphQLObjectType({
     virtual_block_net_limit: { type: GraphQLInt }
   })
 });
-
 const BlockType = new GraphQLObjectType({
   name: "Block",
   fields: () => ({
@@ -50,13 +44,12 @@ const BlockType = new GraphQLObjectType({
     action_mroot: { type: GraphQLString },
     schedule_version: { type: GraphQLInt },
     producer_signature: { type: GraphQLString },
-    actions_count: { type: GraphQLInt },
     transactions: { type: GraphQLList(TransactionType) },
     virtual_block_cpu_limit: { type: GraphQLInt },
-    virtual_block_net_limit: { type: GraphQLInt }
+    virtual_block_net_limit: { type: GraphQLInt },
+    actions_count: { type: GraphQLInt }
   })
 });
-
 const TransactionType = new GraphQLObjectType({
   name: "Transaction",
   fields: () => ({
@@ -66,18 +59,17 @@ const TransactionType = new GraphQLObjectType({
     trx: { type: TrxType }
   })
 });
-
 const TrxType = new GraphQLObjectType({
   name: "Trx",
   fields: () => ({
     id: { type: GraphQLString },
+    signatures: { type: GraphQLList(GraphQLString) },
     compression: { type: GraphQLString },
     packed_context_free_data: { type: GraphQLString },
     packed_trx: { type: GraphQLString },
     transaction: { type: TransactionReceiptType }
   })
 });
-
 const TransactionReceiptType = new GraphQLObjectType({
   name: "TransactionReceipt",
   fields: () => ({
@@ -90,55 +82,73 @@ const TransactionReceiptType = new GraphQLObjectType({
     actions: { type: GraphQLList(ActionType) }
   })
 });
-
 const ActionType = new GraphQLObjectType({
   name: "Action",
   fields: () => ({
     account: { type: GraphQLString },
-    name: { type: GraphQLString }
+    name: { type: GraphQLString },
+    data: { type: GraphQLString }
   })
 });
-
-//----------------------------------------------------HELPERS---------------------------------------------------//
-
-async function getChainInfo() {
+const ActionDataType = new GraphQLObjectType({
+  name: "ActionData",
+  fields: () => ({
+    chain_params_hash: { type: GraphQLString },
+    manifest_id: { type: GraphQLString }
+  })
+});
+const BlockHeaderType = new GraphQLObjectType({
+  name: "BlockHeader",
+  fields: () => ({
+    header: { type: HeaderType }
+  })
+});
+const HeaderType = new GraphQLObjectType({
+  name: "Header",
+  fields: () => ({
+    timestamp: { type: GraphQLString }
+  })
+});
+async function getChainMetadata() {
   try {
     const data = await rpc.get_info();
     return data;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
-
-async function getBlocks(n, lim) {
-  const blocks = [];
-  try {
-    for (let i = 0; i < lim; i++) {
-      blocks.push(await getBlock(n - i));
-    }
-  } catch (err) {
-    console.log(err)
-  }
-
-  return blocks;
-}
-
-async function getBlock(n) {
-  const data = await rpc.get_block(n);
+async function getBlockMetadata(num) {
+  const data = await rpc.get_block(num);
   try {
     const actionsCount = await countActions(data);
-    if (actionsCount) {
-      defineProperty(data, "actions_count", actionsCount);
-    } else {
-      defineProperty(data, "actions_count", 0);
-    }
+    defineProperty(data, "actions_count", actionsCount);
+    delete data.transactions;
     return data;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
-
-//Returns total number of actions in a block
+async function getBlock(num) {
+  const data = await rpc.get_block(num);
+  try {
+    const actionsCount = await countActions(data);
+    defineProperty(data, "actions_count", actionsCount);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function getBlocks(num, limit) {
+  const blocks = [];
+  try {
+    for (let i = 0; i < limit; i++) {
+      blocks.push(await getBlock(num - i));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  return blocks;
+}
 const countActions = block => {
   let count = 0;
   block.transactions.forEach(t => {
@@ -148,52 +158,57 @@ const countActions = block => {
   });
   return count;
 };
-
-//Checks if a "transaction" in a block exists (and therefore has actions)
+//If "transaction" property of trx exists, then a block has actions (all transaction have actions).
 const hasActions = t => {
   if (t.trx.hasOwnProperty("transaction")) {
     return true;
   }
   return false;
 };
-
 const defineProperty = (obj, k, v) => {
   obj[k] = v;
   return obj;
 };
-
-async function getAbi(){
-  const data = await rpc.get_abi('eosio')
-}
-
-getAbi()
-
-//---------------------------------------------------ROOT TYPES--------------------------------------------------------//
-
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
-    getChain: {
+    getChainMetadata: {
       type: ChainType,
       resolve() {
-        return getChainInfo();
+        return getChainMetadata();
+      }
+    },
+    getBlockMetadata: {
+      type: BlockType,
+      args: { block_num: { type: GraphQLInt } },
+      resolve(parent, args) {
+        const num = args.block_num;
+        return getBlockMetadata(num);
       }
     },
     getBlock: {
       type: BlockType,
       args: { block_num: { type: GraphQLInt } },
       resolve(parent, args) {
-        const n = args.block_num;
-        return getBlock(n);
+        const num = args.block_num;
+        return getBlock(num);
       }
     },
     getBlocks: {
       type: GraphQLList(BlockType),
       args: { block_num: { type: GraphQLInt }, limit: { type: GraphQLInt } },
       resolve(parent, args) {
-        const n = args.block_num;
-        const lim = args.limit;
-        return getBlocks(n, lim);
+        const num = args.block_num;
+        const limit = args.limit;
+        return getBlocks(num, limit);
+      }
+    },
+    getBlockHeader: {
+      type: BlockHeaderType,
+      args: { block_num: { type: GraphQLInt } },
+      resolve(parent, args) {
+        const num = args.block_num;
+        return getBlockHeader(num);
       }
     }
   }
